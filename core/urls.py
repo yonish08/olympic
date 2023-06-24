080ce0ca272d8485eb819f0aeb0b1054a8cfb98c
sundartamang <sundartamang6@gmail.com>
2023-06-24 17:54:57 +0545

    client tempalte url and html set up finished

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,30 @@
 from django.urls import path
 from .views import (
-    DashboardView,
     AdminDashboardView,
+    # client views
+    DashboardView,
+    AboutusView,
+    FixtureView,
+    HightlightView,
+    LiveView,
+    NewsView,
+    NewsDetailView,
+    PlayerView,
+    SportView
 )
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard'),
     path('admin-dashboard/', AdminDashboardView.as_view(), name='admin-dashboard'),
+    # client views
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('about-us', AboutusView.as_view(), name='about_us'),
+    path('fixture', FixtureView.as_view(), name='fixture'),
+    path('highlight', HightlightView.as_view(), name='highlight'),
+    path('live', LiveView.as_view(), name='live'),
+    path('news', NewsView.as_view(), name='news'),
+    path('news-detail', NewsDetailView.as_view(), name='news_detail'),
+    path('player', PlayerView.as_view(), name='player'),
+    path('sport', SportView.as_view(), name='sport'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,15 +6,58 @@ from core.mixins import UserRequiredMixin, AdminRequiredMixin
 
 
 # Create your views here.
+
+# admin dashboard
+class AdminDashboardView(AdminRequiredMixin, TemplateView):
+    template_name = 'siteadmin/base.html'
+    login_url = 'user:login'
+
+
+
+# CLIENT SITE VIEW
 # dashboard
 class DashboardView(UserRequiredMixin, TemplateView):
     template_name = 'client/dashboard.html'
     login_url = 'user:login'
 
 
-# admin dashboard
-class AdminDashboardView(AdminRequiredMixin, TemplateView):
-    template_name = 'siteadmin/base.html'
+class AboutusView(UserRequiredMixin, TemplateView):
+    template_name = 'client/aboutus.html'
+    login_url = 'user:login'
+
+
+class FixtureView(UserRequiredMixin, TemplateView):
+    template_name = 'client/fixtures.html'
+    login_url = 'user:login'
+
+
+class HightlightView(UserRequiredMixin, TemplateView):
+    template_name = 'client/highlight.html'
+    login_url = 'user:login'
+
+
+class LiveView(UserRequiredMixin, TemplateView):
+    template_name = 'client/live.html'
+    login_url = 'user:login'
+
+
+class NewsView(UserRequiredMixin, TemplateView):
+    template_name = 'client/news.html'
+    login_url = 'user:login'
+
+
+class NewsDetailView(UserRequiredMixin, TemplateView):
+    template_name = 'client/newsdetail.html'
+    login_url = 'user:login'
+
+
+class PlayerView(UserRequiredMixin, TemplateView):
+    template_name = 'client/player.html'
+    login_url = 'user:login'
+
+
+class SportView(UserRequiredMixin, TemplateView):
+    template_name = 'client/sport.html'
     login_url = 'user:login'
 
 
